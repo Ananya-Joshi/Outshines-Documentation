3bc22f6bcdc18fd786acb1a53c9c4ff75d580d0b
Ananya-Joshi <ananya@ananya.co>
2023-08-16 04:02:40 +0800

    Added comments matching algorithm lines.

--- scoring_functions.py
+++ scoring_functions.py
@@ -20,19 +20,19 @@ def eval_recent_distribution(i, t_day_num, evd_total, df_ts, day):
     if i < j:
         start_ind = min(i, t_day_num//2)
         dist_total = evd_total.iloc[:, np.r_[i-start_ind:i,
-                    i+1:i+(t_day_num-start_ind)]].stack().dropna()
+                    i+1:i+(t_day_num-start_ind)]].stack().dropna() #Line 3 in Alg
     else:
         end_ind = min(t_day_num//2, j-1)
         dist_total = evd_total.iloc[:, np.r_[i-(t_day_num-end_ind):i, i+1:i+1+end_ind]]
         dist_total = dist_total.stack().dropna()
-    series = df_ts.test_stat_total.apply(lambda x: (x > dist_total).sum()/dist_total.shape[0])
+    series = df_ts.test_stat_total.apply(lambda x: (x > dist_total).sum()/dist_total.shape[0]) # Line 7 in Alg
     series.name = day
     series.index = df_ts.geo_key_id
     return series, dist_total.shape[0]
 
 def outshines_score(ind_ts,  evd_total, const,t_day_num=28):
-    """Evaluate out of sample points to comparison EVD Distribution.
-
+    """Evaluate out of sample points to comparison EVD Distribution.   
+    
     Args:
         ind_ts: pd.DataFrame of indicator test statistics
         evd_total: pd.DataFrame of EVD distributions per day
@@ -44,7 +44,7 @@ def outshines_score(ind_ts,  evd_total, const,t_day_num=28):
     total_list = []
     series = pd.Series(dtype='float64')
     for i, (day,df_ts) in enumerate(ind_ts.sort_values(['time_value',
-                                    'geo_key_id']).groupby('time_value')):
+                                    'geo_key_id']).groupby('time_value')): 
         df_ts = df_ts.reset_index(drop=True).drop_duplicates('geo_key_id')
         series, dist_shape = eval_recent_distribution(i, t_day_num, evd_total, df_ts, day)
         total_list.append(series)
@@ -59,19 +59,27 @@ def outshines_score(ind_ts,  evd_total, const,t_day_num=28):
 def outshines_score_formatter(ts_df):
     """Outshines scoring method.
 
+    Please follow along with line numbers from Algorithm 1 in the paper across the functions. 
+    Note the following changes: 
+    Line 5 includes absolute value to ensure that the 
+    most extreme test statistics are returned regardless of direction. 
+
+    Line 7's scaling factor is implemented manually depending on if there are 
+    different indicators being used with this method.
+
     Input: ts_df: pd.DataFrame of test statistics
     Output: pd.DataFrame of Outshines Scores
     """
     mega_list = pd.read_csv('source/siblings_groups.csv', index_col=0).values.tolist()
     evd_dist = []
-    for i, set_ml in enumerate(pd.Series(mega_list)):
+    for i, set_ml in enumerate(pd.Series(mega_list)): #Line 1 in Alg
         set_ml = pd.Series(list(set(set_ml))).replace('nan', np.nan).dropna().astype(int)
         set_ml = set_ml[set_ml.isin(ts_df.columns)]
         if len(set_ml) > 0:
-            ts_df_stack = ts_df[set_ml].abs().max(axis=1)
+            ts_df_stack = ts_df[set_ml].abs().max(axis=1) #Line 5 in Alg
             ts_df_stack.name=i
             evd_dist.append(ts_df_stack)
-    evd_dist = pd.concat(evd_dist, axis=1)
+    evd_dist = pd.concat(evd_dist, axis=1) # Line 6 in Alg
     ts_df2 = ts_df.stack().reset_index().rename(columns={0:'test_stat_total',
                                                            'level_1':'geo_key_id'})
     ts_df2.time_value = pd.to_datetime(ts_df2.time_value)
